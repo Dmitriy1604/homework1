daa4f2440cf988cd5ec890a9ebb5b73ca1a85c4b
Dmitriy1604 <Dimon312-91@mail.ru>
2022-07-07 19:15:10 +0300

    Initial commit

---
+++ swe.java
@@ -0,0 +1,9 @@
+public class swe {
+        public static void main(String[] args) {
+            System.out.println("поверни направо");
+            System.out.println("зачем");
+            System.out.println("я самурай у меня свой путь");
+
+
+        }
+    }
